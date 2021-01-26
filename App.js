b80d906d78a38b15c86b9e4fb15eefd6fd6ce90b
Zartoru <bassez.claude97@gmail.com>
2021-01-26 12:30:58 +0100

    Début du projet

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+
+  const [icon, setIcon] = useState('01d');
+  const [currentDate, setCurrentDate] = useState('');
+  const [temperature, setTemperature] = useState('0')
+
+  useEffect(() => {
+    const date = new Date().getDate(); //Current Date
+    const month = new Date().getMonth() + 1; //Current Month
+    const year = new Date().getFullYear(); //Current Year
+    
+    setCurrentDate(
+      date + '/' + month + '/' + year
+    )
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.today} >
+        <Text style={styles.today__title}>{currentDate}</Text>
+        <Image
+          style={styles.today__icon}
+          source={require(
+            `./images/${icon}.png`
+          )} 
+          />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -18,4 +40,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  today: {
+    backgroundColor: "blue",
+    margin: 0,
+    height: 250,
+  },
+  today__title: {
+    marginTop: 50,
+    color: "white",
+  },
+  today__icon: {
+    height: 100,
+    width: 200,
+    marginRight: 50,
+    marginLeft: "auto",
+  }
 });
